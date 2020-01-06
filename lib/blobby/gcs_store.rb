815b4e0ff12efd5a057f8be6943907b3b3f7ad28
Jerome Paul <jerome.paul@greensync.com.au>
2020-01-06 11:40:58 +1100

    Implement a blobby API wrapper for Google Cloud Storage

--- lib/blobby/gcs_store.rb
+++ lib/blobby/gcs_store.rb
@@ -1,6 +1,101 @@
 # frozen_string_literal: true
 
+require 'google/cloud/storage'
+
 module Blobby
+  # A BLOB store backed by Google Cloud Storage.
+  #
   class GCSStore
+
+    def self.from_uri(uri)
+      uri = URI(uri)
+      raise ArgumentError, "invalid GCS address: #{uri}" unless uri.scheme == 'gs'
+
+      bucket_name = uri.host
+      prefix = uri.path.sub(%r{\A/}, '').sub(%r{/\Z}, '')
+      raise ArgumentError, 'no bucket specified' if bucket_name.nil?
+
+      store = new(bucket_name)
+      store = KeyTransformingStore.new(store) { |key| prefix + '/' + key } unless prefix.empty?
+      store
+    end
+
+    def initialize(bucket_name, gcs_options: {})
+      @bucket_name = bucket_name
+      @gcs_options = gcs_options
+    end
+
+    def available?
+      bucket.exists?
+    end
+
+    def [](key)
+      KeyConstraint.must_allow!(key)
+      StoredObject.new(bucket, key)
+    end
+
+    class StoredObject
+
+      def initialize(bucket, key)
+        @bucket = bucket
+        @key = key
+      end
+
+      # https://googleapis.dev/ruby/google-cloud-storage/latest/Google/Cloud/Storage/File.html#exists%3F-instance_method
+      # bucket.file(key) returns nil, if the file does not exist!
+      def exists?
+        gcs_file.nil? ? false : gcs_file.exists?
+      end
+
+      # https://googleapis.dev/ruby/google-cloud-storage/latest/Google/Cloud/Storage/File.html#download-instance_method
+      def read
+        return nil unless exists?
+
+        content = gcs_file.download
+        if block_given?
+          yield content
+          nil
+        else
+          content
+        end
+      end
+
+      # https://googleapis.dev/ruby/google-cloud-storage/latest/Google/Cloud/Storage/Bucket.html#create_file-instance_method
+      def write(content)
+        bucket.create_file(content, key)
+        nil
+      end
+
+      # https://googleapis.dev/ruby/google-cloud-storage/latest/Google/Cloud/Storage/File.html#delete-instance_method
+      def delete
+        return false unless exists?
+
+        gcs_file.delete
+      end
+
+      private
+
+      attr_reader :bucket, :key
+
+      def gcs_file
+        @gcs_file ||= bucket.file(key)
+      end
+
+    end
+
+    private
+
+    attr_reader :bucket_name, :gcs_options
+
+    def bucket
+      @bucket ||= gcs_client.bucket(bucket_name)
+    end
+
+    def gcs_client
+      @gcs_client ||= Google::Cloud::Storage.new(gcs_options)
+    end
+
   end
+
+  register_store_factory 'gs', GCSStore
 end
--- spec/blobby/gcs_store_spec.rb
+++ spec/blobby/gcs_store_spec.rb
@@ -3,7 +3,63 @@
 require 'blobby-gcs'
 
 RSpec.describe Blobby::GCSStore do
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'when accessing a public bucket' do
+    before(:all) do
+      @gcs_store = Blobby.store('gs://gcp-public-data-landsat')
+    end
+
+    describe '#available?' do
+      it 'is true' do
+        expect(@gcs_store).to be_available
+      end
+    end
+
+    describe '#[]' do
+      context 'with a key that exists in the bucket' do
+        before(:all) do
+          @gcs_file = @gcs_store['LT05/PRE/001/001/LT50010011985144KIS00/LT50010011985144KIS00_MTL.txt']
+        end
+
+        describe '#exists?' do
+          it 'is true' do
+            expect(@gcs_file).to be_exist
+          end
+        end
+
+        describe '#read' do
+          it 'downloads the content' do
+            content = @gcs_file.read
+            content.rewind if content.respond_to? :rewind
+            expect(content.read).to include('GROUP = L1_METADATA_FILE')
+          end
+        end
+
+        describe '#delete' do
+          xit 'is not tested'
+        end
+
+        describe '#write' do
+          xit 'is not tested'
+        end
+      end
+
+      context 'with a key that does not exist in the bucket' do
+        before(:all) do
+          @gcs_file = @gcs_store['does/not/exist.txt']
+        end
+
+        describe '#exists?' do
+          it 'is false' do
+            expect(@gcs_file).not_to be_exist
+          end
+        end
+
+        describe '#read' do
+          it 'returns nil' do
+            expect(@gcs_file.read).to be_nil
+          end
+        end
+      end
+    end
   end
 end
